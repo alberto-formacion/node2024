23a3bc63c19d4355cfc9fe192b83ed14a8da6f9f
Alberto Garcia de Cortazar <acortazar@centrosanluis.com>
2024-03-16 13:10:56 +0100

    Cambio para arreglar el problema al listar por parametro

--- moduloFS/listarDirectorio.js
+++ moduloFS/listarDirectorio.js
@@ -1,9 +1,16 @@
-const fs = require('node:fs/promises')
+const fs = require('node:fs/promises');
 
-fs.readdir('.')
+const directory = process.argv[2] ?? "./";
+
+fs.readdir(directory)
   .then(files => {
     files.forEach(file => {
-      console.log(file)
+      fs.stat(directory + "\\" + file).then(stat=>{
+        const directory = stat.isDirectory()?"<DIR>":"";
+        const size = (stat.size / 1024).toFixed(2);
+        console.log(file.padEnd(60) + directory.padEnd(10) + (stat.isDirectory()?"":(size + " MB")));
+      })
+      
     })
   })
   .catch(err => {
