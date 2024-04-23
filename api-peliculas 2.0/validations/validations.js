aaf15e4801e4685a8e1d7fc9eac63cf92f7ad053
Alberto <acortazar@centrosanluis.com>
2024-04-23 20:27:34 +0200

    cambios en las validaciones

--- api-peliculas 2.0/validations/validations.js
+++ api-peliculas 2.0/validations/validations.js
@@ -3,10 +3,15 @@ const { z } = require('zod');
 // Esquema de validación para películas
 const peliculaSchema = z.object({
     titulo: z.string().min(1),
-    anyo: z.number().int().positive(),
-    directores: z.array(z.string()).min(1),
-    actores: z.array(z.string()).min(1),
-    sinopsis: z.string().min(1),
+    anyo: z.number({
+        required_error: "El año es obligatorio",
+        invalid_type_error: "El año tiene que ser un numero",
+      })
+      .int({message: "El año tiene que ser un entero"})
+      .min(1895, {message: "El año tiene que ser como minimo 1895"}),
+    directores: z.array(z.string().min(5)).min(1),
+    actores: z.array(z.string().min(5)).min(1),
+    sinopsis: z.string().min(20),
     duracion: z.number().int().positive()
 });
 
